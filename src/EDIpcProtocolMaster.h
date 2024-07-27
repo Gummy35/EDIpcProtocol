e6099216b0309247930dabb8c35f75293177ab4a
Gummy35 <gummy35@nomail.com>
2024-07-27 12:27:22 +0200

    Add reboot command, add location preliminary tests

--- src/EDIpcProtocolMaster.h
+++ src/EDIpcProtocolMaster.h
@@ -10,12 +10,17 @@ class EDIpcProtocolMaster {
 public:
     static EDIpcProtocolMaster* instance;
 
+
+    char LocationSystemName[21];
+    char LocationStationName[21];
+
     EDIpcProtocolMaster(I2CDevice* comMcu);
     bool begin();
     void setAxis(int32_t x, int32_t y, int32_t z, int32_t rx, int32_t ry, int32_t rz);
     void sendChanges();
     uint8_t retrieveChanges();
     bool pingSlave();
+    bool resetSlave();
     bool sendKey(KeyEvent event);
 
 protected:
@@ -28,6 +33,7 @@ protected:
     bool _getGameInfo();
     bool _getKeypadConfig();
     bool _getWifiCredentials();
+    bool _getLocationData();
 
     unsigned long _lastUpdate = 0;
 };
